e8627006a01fe7f08a2eec370222fd35f998f26c
Jaywesterlow <jaywesterlow71@gmail.com>
2022-01-14 11:51:08 +0100

    Vars & first hooks

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+import axios from 'axios';
+import './App.css';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+
+export default App;
