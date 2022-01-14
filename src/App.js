9675c47c95d0428f6b92309c25acd88f3a1f76ea
Jaywesterlow <jaywesterlow71@gmail.com>
2022-01-14 11:54:10 +0100

    Post system

--- src/App.js
+++ src/App.js
@@ -21,5 +21,18 @@ const App = () => {
     fetchPosts();
   }, []);
 
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  return (
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      
+    </div>
+  );
+};
 
 export default App;
